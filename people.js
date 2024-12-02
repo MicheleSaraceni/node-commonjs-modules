2554bed4af48be6b6c631d5a4d70487b6709e8bc
MicheleSaraceni <michele.saraceni23@email.com>
2024-12-02 16:42:04 +0100

    end basic exercise

--- people.js
+++ people.js
@@ -3,3 +3,12 @@ const hobbies = require("./hobbies.js");
 
 console.log(names("Michele", "Saraceni"));
 console.log(hobbies("GDR", "One Piece", "Amici"));
+
+function person() {
+    return {
+        name: names("Michele", "Saraceni"),
+        hobbie: hobbies("GDR", "One Piece", "Amici")
+    }
+}
+
+console.log(person());
